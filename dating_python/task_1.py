ede6f40d2d02b4332cf6bf25638037ce6d100e4e
Valentine <dialog.valentine@gmail.com>
2020-08-29 03:22:11 +0900

    all tasks solved

---
+++ dating_python/task_1.py
@@ -0,0 +1,8 @@
+a = 'First variable'
+b = ['Second variable', a, 1, 2]
+print(b[2], b[1], b[3], b[0])
+
+user_name = input('Ваше имя: ')
+user_age = input('Ваш возраст: ')
+user_info = f'Вас зовут {user_name} и вам {user_age} лет'
+print(user_info)
