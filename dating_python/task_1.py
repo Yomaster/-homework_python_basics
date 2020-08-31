cedc3aaf613c953ed1b3fa2add3491348cdc1e3d
Valentine <dialog.valentine@gmail.com>
2020-08-31 15:27:36 +0900

    branch cleanup

--- dating_python/task_1.py
+++
@@ -1,8 +0,0 @@
-a = 'First variable'
-b = ['Second variable', a, 1, 2]
-print(b[2], b[1], b[3], b[0])
-
-user_name = input('Ваше имя: ')
-user_age = input('Ваш возраст: ')
-user_info = f'Вас зовут {user_name} и вам {user_age} лет'
-print(user_info)
